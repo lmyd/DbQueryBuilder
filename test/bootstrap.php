64ddeebf96222358fedf64b29833644b81f024db
Lukasz Mydlarski <lukasz.mydlarski@fru.pl>
2016-07-07 19:30:32 +0200

    repo setings

---
+++ test/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+ini_set('display_errors', true);
+error_reporting(E_ALL & ~E_STRICT);
+
+define('UNIT_TEST_YOURAPPLICATION_TESTSUITE', true);
+
+// Require composer autoloader
+require __DIR__.'/../vendor/autoload.php';
